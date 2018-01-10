84189706c94ec5837d4f22144a2fcadc511d11c5
wellingtondellamura <della.mura@gmail.com>
2018-01-10 10:57:17 -0200

    The resume button

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
 var btnStart = document.getElementById("start");
 var btnReset = document.getElementById("reset");
+var btnContinue = document.getElementById("continue");
 var btnStop = document.getElementById("stop");
+
 var timer = {
    value : new Date(),
    started : false,
@@ -10,43 +12,72 @@ var timer = {
    getMilliseconds : function(){return this.value.getTime() - new Date().getTime()}
 }
 
+var DEFAULT_TICK = 20;
+
 btnStart.addEventListener("click", function(){
+   showContinue(false);
    startTimer();
 });
 
 btnStop.addEventListener("click", function(){
+   showContinue(true);
    stopTimer();
 });
 
+btnContinue.addEventListener("click", function(){
+   showContinue(false);
+   resumeTimer();
+});
+
 btnReset.addEventListener("click", function(){
+   showContinue(false);
    resetTimer();
 });
+
+function showContinue(b){
+   if (b){
+         btnContinue.style.visibility = "visible";
+         btnContinue.style.display = "inline";
+         btnStop.style.visibility = "hidden";
+         btnStop.style.display = "none";
+   }else{
+         btnContinue.style.visibility = "hidden";
+         btnContinue.style.display = "none";
+         btnStop.style.visibility = "visible";
+         btnStop.style.display = "inline";
+   }
+}
 
 function startTimer(){
    timer.value = new Date();
    timer.started = true;
-   t = setTimeout(tick, 1);
+   tick();
 }
 
 function stopTimer(){
    timer.started = false;
 }
 
+function resumeTimer(){
+   timer.started = true;
+   tick();
+}
+
 function resetTimer(){
    timer.started = false;
    timer.value = new Date();
-   setTimeout(function(){updateTimer(0);}, 10);
+   setTimeout(function(){updateTimer(0, timer.pomodoro);}, DEFAULT_TICK*2);
 }
 
 function tick(){
    if (timer.started) {
-      setTimeout(tick, 1);
+      setTimeout(tick, DEFAULT_TICK);
    }
-   updateTimer(timer.getMilliseconds());
+   updateTimer(timer.getMilliseconds(), timer.pomodoro);
 }
 
-function updateTimer(m){
-   m = m + 25*60*1000;
+function updateTimer(m, t){
+   m = m + t*60*1000;
    var mil = m % 1000;
    var s = Math.floor(m / 1000);
    var sec = s % 60;
@@ -62,6 +93,5 @@ Number.prototype.pad = function(size) {
   while (s.length < (size || 2)) {s = "0" + s;}
   return s;
 }
-
 
 resetTimer();
